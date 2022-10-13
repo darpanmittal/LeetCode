8ea40fb99131b279a43752359490c5fdbb0008a7
Darpan Kumar Mittal <89724797+darpanmittal@users.noreply.github.com>
2022-10-13 21:47:46 +0530

    Update 0237-delete-node-in-a-linked-list.cpp

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -10,5 +10,19 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
         *node=*(node->next);
+        
+     
     }
 };
+
+
+
+//ListNode *temp=node->next;
+        //while(temp!=NULL)
+      //  {
+      //      node->val=temp->val;
+         //   if(temp->next!=NULL)
+          //  node=node->next;
+         //   temp=temp->next;
+       // }
+    //    node->next=NULL;
